27f4c45898ef2235ed1ee52a16e9726a14871bbe
于智勇 <zhiyong.yu@ytever.com>
2023-05-25 17:09:59 +0800

    feat: build

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,5 @@
 import DefaultTheme from 'vitepress/theme'
-import {createPinia} from 'pinia'
+// import {createPinia} from 'pinia'
 import ElementPlus from 'element-plus'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import {useComponents} from './useComponents'
@@ -10,10 +10,11 @@ import './styles/index.css'
 export default {
     ...DefaultTheme,
     enhanceApp(ctx) {
-        const pinia = createPinia()
+        // 引三方组件
+        // const pinia = createPinia()
         DefaultTheme.enhanceApp(ctx)
 
-        ctx.app.use(pinia)
+        // ctx.app.use(pinia)
         ctx.app.use(ElementPlus)
         // 全局加载 自定义组件
         useComponents(ctx.app)
