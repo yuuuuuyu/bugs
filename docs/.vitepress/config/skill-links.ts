c91a945ccf9298be45d08536f8019d9eef4d2c0e
于智勇 <zhiyong.yu@ytever.com>
2023-12-16 15:53:32 +0800

    feat: 解决ERR_PNPM_NO_MATCHING_VERSION

--- docs/.vitepress/config/skill-links.ts
+++ docs/.vitepress/config/skill-links.ts
@@ -1,75 +1,87 @@
 const skills = [
-    {
-        text: 'Vue3.x',
-        collapsible: true,
-        collapsed: false,
-        items: [
-            {
-                text: 'Teleport',
-                link: 'skills/vue3.x/teleport'
-            }, {
-                text: 'Pinia持久化',
-                link: 'skills/vue3.x/pinia'
-            },
-        ]
-    }, {
-        text: 'Beeboat',
-        collapsible: true,
-        collapsed: false,
-        items: [
-            {
-                text: '使用代码片段重写表格列',
-                link: 'skills/beeboat/1'
-            },
-            {
-                text: '使用代码片段实现动态列',
-                link: 'skills/beeboat/2'
-            },
-            {
-                text: '使用代码片段实现表头合并',
-                link: 'skills/beeboat/3'
-            },
-            {
-                text: '重写组件事件/属性',
-                link: 'skills/beeboat/4'
-            }, {
-                text: '重写组件事件使用emit',
-                link: 'skills/beeboat/5'
-            },
-        ]
-    }, {
-        text: '插件',
-        collapsible: true,
-        collapsed: false,
-        items: [
-            {
-                text: 'DirverJs功能步骤引导',
-                link: 'skills/plugins/dirverjs/dirverjs'
-            }, {
-                text: 'LuckSheet在线excel/导入/导出',
-                link: ''
-            },
-        ]
-    }, {
-        text: 'OPL',
-        collapsible: true,
-        collapsed: false,
-        items: [
-            {
-                text: '参数归一化',
-                link: 'skills/opl/参数归一化.md'
-            },
-        ]
-    }, {
-        text: 'Vite',
-        collapsible: true,
-        collapsed: false,
-        items: [
-            {
-                text: '处理自定义环境变量',
-                link: 'skills/opl/参数归一化.md'
-            },
-        ]
-    }
+  {
+    text: "Vue3.x",
+    collapsible: true,
+    collapsed: false,
+    items: [
+      {
+        text: "Teleport",
+        link: "skills/vue3.x/teleport",
+      },
+      {
+        text: "Pinia持久化",
+        link: "skills/vue3.x/pinia",
+      },
+      {
+        text: "报错ERR_PNPM_NO_MATCHING_VERSION",
+        link: "skills/vue3.x/question/1",
+      },
+    ],
+  },
+  {
+    text: "Beeboat",
+    collapsible: true,
+    collapsed: false,
+    items: [
+      {
+        text: "使用代码片段重写表格列",
+        link: "skills/beeboat/1",
+      },
+      {
+        text: "使用代码片段实现动态列",
+        link: "skills/beeboat/2",
+      },
+      {
+        text: "使用代码片段实现表头合并",
+        link: "skills/beeboat/3",
+      },
+      {
+        text: "重写组件事件/属性",
+        link: "skills/beeboat/4",
+      },
+      {
+        text: "重写组件事件使用emit",
+        link: "skills/beeboat/5",
+      },
+    ],
+  },
+  {
+    text: "插件",
+    collapsible: true,
+    collapsed: false,
+    items: [
+      {
+        text: "DirverJs功能步骤引导",
+        link: "skills/plugins/dirverjs/dirverjs",
+      },
+      {
+        text: "LuckSheet在线excel/导入/导出",
+        link: "",
+      },
+    ],
+  },
+  {
+    text: "OPL",
+    collapsible: true,
+    collapsed: false,
+    items: [
+      {
+        text: "参数归一化",
+        link: "skills/opl/参数归一化.md",
+      },
+    ],
+  },
+  {
+    text: "Vite",
+    collapsible: true,
+    collapsed: false,
+    items: [
+      {
+        text: "处理自定义环境变量",
+        link: "skills/opl/参数归一化.md",
+      },
+    ],
+  },
 ]
 export default skills
+
