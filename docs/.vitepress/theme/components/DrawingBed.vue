2d13e537186ee792016ee22b110cf024d26fe4f8
于智勇 <zhiyong.yu@ytever.com>
2024-04-04 09:41:35 +0800

    fix: 图床迁移至Jenkins

--- docs/.vitepress/theme/components/DrawingBed.vue
+++ docs/.vitepress/theme/components/DrawingBed.vue
@@ -23,7 +23,7 @@ const props = defineProps({
 })
 
 const fullUrl = computed(() => {
-  const prefix = "https://yuuuuuyu.gitee.io/drawing-bed/"
+  const prefix = "https://ebugs.l2.ttut.cc/drawing-bed/"
   return `${prefix}${props.src}`
 })
 </script>
