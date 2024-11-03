e485a28152b13e3046ae3173a1b434121ad2a817
zhiyong.yu <15684187702@163.com>
2024-11-03 20:19:58 +0800

    feat: 加了历史搬砖模块

---
+++ docs/.vitepress/theme/utils/works.ts
@@ -0,0 +1,19 @@
+export const works = [
+  {
+    title: "生产排程",
+    name: "scheduling-app",
+    url: "https://ebugs.l2.ttut.cc/schedule/",
+    desc: "基于vue3、elementplus",
+    bg: "https://ebugs.l2.ttut.cc/drawing-bed/works/scheduling.png",
+    github: "https://github.com/yuuuuuyu/scheduling-app.git",
+  },
+  {
+    title: "生产排程",
+    name: "scheduling-app",
+    url: "https://ebugs.l2.ttut.cc/schedule/",
+    desc: "基于vue3、elementplus",
+    bg: "https://ebugs.l2.ttut.cc/drawing-bed/works/scheduling.png",
+    github: "https://github.com/yuuuuuyu/scheduling-app.git",
+  },
+]
+
---
+++ docs/works.md
@@ -0,0 +1,14 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: page
+editLink: false
+lastUpdated: false
+isNoComment: true
+isNoBackBtn: true
+---
+
+<WorksPage />
+
+<script lang="ts" setup>
+import WorksPage from './.vitepress/theme/views/WorksPage.vue'
+</script>
